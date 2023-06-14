8d46b911db1ffc8509719b2c427e9dd096727b87
Co-Ing <127190327+coing-ye@users.noreply.github.com>
2023-06-15 00:39:01 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 52 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,12 @@
+import sys
+N = int(sys.stdin.readline())
+
+count = [0]*(N+1)
+count[1] = 1
+
+if N>1:
+    count[2] = 3
+
+for i in range(3,N+1):
+    count[i] = count[i-1]+count[i-2]*2
+print(count[N]%10007)
