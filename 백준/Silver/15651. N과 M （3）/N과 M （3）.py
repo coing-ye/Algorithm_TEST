76258376316fb915c00f5e90b4aad6f0a34c3356
Co-Ing <127190327+coing-ye@users.noreply.github.com>
2023-06-18 23:28:37 +0900

    [Silver III] Title: N과 M (3), Time: 544 ms, Memory: 192772 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def recur(a,b,exp,sol):
+    if b==0:
+        sol.append(exp)
+        return
+    for i in range(len(a)):
+        #tempset=a[:i]+a[i+1:]
+        tempset=a
+        exp=exp+str(a[i])
+        recur(tempset,b-1,exp,sol)
+        exp = exp[:-1]
+    return sol
+
+a, b = map(int,sys.stdin.readline().split())
+numset = []
+for i in range(1,a+1):
+    numset.append(i)
+solset = recur(numset,b,'',[])
+
+for i in solset:
+    solexp = ''
+    for j in range(0,len(i)-1):
+        solexp = solexp + i[j] + ' '
+    solexp = solexp + i[-1]
+    print(solexp)
