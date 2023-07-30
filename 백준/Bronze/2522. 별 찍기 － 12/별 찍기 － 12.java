af82db1c770cc114c1f69fe21eee6e5b8ec429ce
coingye96 <127190327+coing-ye@users.noreply.github.com>
2023-07-30 15:01:02 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 100 ms, Memory: 13004 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		int N = Integer.parseInt(st.nextToken());
+		
+		for(int i=1;i<=N;i++) {
+			StringBuilder sb = new StringBuilder();
+			for(int j=0;j<N-i;j++) {
+				sb.append(" ");
+			}
+			for(int j=0;j<i;j++) {
+				sb.append("*");
+			}
+			System.out.println(sb);
+		}
+		for(int i=N-1;i>0;i--) {
+			StringBuilder sb = new StringBuilder();
+			for(int j=0;j<N-i;j++) {
+				sb.append(" ");
+			}
+			for(int j=0;j<i;j++) {
+				sb.append("*");
+			}
+			System.out.println(sb);
+		}
+	}
+
+}
