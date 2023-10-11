37c3d09f3bf0cf0c53650ee16b8f9274188a58af
coingye96 <127190327+coing-ye@users.noreply.github.com>
2023-10-11 17:06:46 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,10 @@
+import math
+def solution(brown, yellow):
+    answer = [0]*2
+    total = brown+yellow
+    for i in range(3,int(math.sqrt(total))+1):
+        if((i-2)*((total//i)-2) == yellow):
+            answer[0] = total//i
+            answer[1] = i
+            break
+    return answer
